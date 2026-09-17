1876ad95a86878644a48a70499b33cc0d2970306
Mouniridlae1302 <idlkhliftmonir@gmail.com>
2026-09-17 16:02:19 +0100

    variables

---
+++ sas.js
@@ -0,0 +1,92 @@
+const prompt = require('prompt-sync')();
+/*
+
+
+let nom = prompt ("votre nom");
+let prénom = prompt ("votre prénom");
+let age = prompt ("ton age");
+let sexe = prompt ("votre sexe");
+let email = prompt ("votre adresse email");
+
+console.log("hello",nom , prénom ,"your age is ",age ,"you are ",sexe , "your email is " , email )
+
+
+let c = parseFloat(prompt("transfforme tempurature : "));
+console.log(`${c}°C correspond à ${k}K`);
+let km =parseInt(prompt( " calculer distencs :" ));
+let yards = km *  1093.61;
+console.log(`${yards} correspond à ${km}`);
+
+
+let kmprh = parseFloat(prompt("calcul Kmprh :" ));
+let mprs = kmprh * 0.27778;
+console.log(`${mprs} correspond à ${kmprh}`);
+
+let C = parseFloat(prompt("musure C:"));
+
+
+let k = c + 273.15;
+if ( C < 0){
+    console.log('solide');
+}else if ( 0 <=  C && C < 100 ) {
+    console.log('liquide');
+
+}else{
+    console.log('gaz');
+}
+
+
+let A = parseInt(prompt("entrez a:"));
+let B = parseInt(prompt( "entrez b:"));
+let add = A + B ;
+let moins = A - B ;
+let multiple = A * B ;
+let div = A / B ;
+
+console.log("add =",add );
+console.log("moins =",moins );
+console.log(" multiple =", multiple );
+console.log("div =",div );
+
+
+let M = parseInt(prompt("entrez nombre:"));
+let M2 = parseInt(prompt("entrez nombre:"));
+let M3 = parseInt(prompt("entrez nombre:"));
+let moyenne = (M * 2 , M2 * 3 , M3 * 5)/10;
+console.log(moyenne);
+
+let a = parseInt(prompt("entrez nombre :"));
+let b = parseInt(prompt("entrez nombre :"));
+let c = parseInt(prompt("entrez nombre : "));
+
+let moyenne = ( a * b * c)**(1/3);
+
+console.log(moyenne);
+
+let x1 = parseInt(prompt("donnez nombre:"));
+let x2 = parseInt(prompt("donnez nombre:"));
+let y1 = parseInt(prompt("donnez nombre:"));
+let y2 = parseInt(prompt("donnez nombre:"));
+let z1 = parseInt(prompt("donnez nombre:"));
+let z2 = parseInt(prompt("donnez nombre:"));
+let Distance = Math.sqrt((x2-x1)**2 + (y2-y1)**2 + (z2-z1)**2);
+console.log(Distance);
+
+
+let longueur = parseFloat(prompt("donnez nombre:"));
+let largeur = parseFloat(prompt("donnez nombre:"));
+let Surface = longueur * largeur;
+console.log(Surface);
+
+*/
+
+
+
+
+
+
+
+
+
+
+
